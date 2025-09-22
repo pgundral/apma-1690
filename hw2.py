398a890681b815db189b21e24de36a4f9e3a1818
Pranav Gundrala <pranavgundrala@Pranavs-MacBook-Air.local>
2025-09-22 13:32:35 -0400

    func done

--- hw2.py
+++ hw2.py
@@ -1,3 +1,14 @@
 ## PRANAV GUNDRALA ## APMA1690 ## FALL 2025 ##
 import numpy as np
+from numpy import random
 
+def compute_pi(N:int, seed:int=42):
+    random.seed(seed)
+    points = random.uniform(-1, 1, N*2)
+    points = np.reshape(points, (N, 2))
+
+    # inside unit circle C
+    C = points[np.sqrt(points[:,0]**2 + points[:,1]**2) <= 1, :]
+    pi = (C.shape[0]/N)*4
+
+    return pi
